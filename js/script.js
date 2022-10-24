5a76590ec1a45a02ebc2d484f27bfc791fa8a14f
SimonGideon <simongideon918@gmail.com>
2022-10-25 00:44:01 +0300

    Remove book function

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 const books = [
     {
+        id: 1,
         title: 'River and the source',
         author: 'Margeret Ogola'
     },
     {
+        id: 2,
         title: 'The Caucasian Chalk Circle',
         author: 'Bertolt Brecht'
     }
 ];
 
 // adding a book
-function addBook (title, author){
+let addBook = (title, author) => {
     books.push({
         title: `${title}`,
         author: `${author}`
     })
 }
+// remove a book in the shelve
+const loop = ()=>{
+    for(let i=0; i<books.length; i++){
+        return i;
+    }
+}
+let id = books.length +1;
+function removeBook(drop){
+    const filt = books.filter((a,i)=>{
+        if(drop == a.id){
+            books.splice(i, 1);
+        }
+    });
+    loop
+}
+
+removeBook(1);
+console.log(books);
